11647a0acd9d4868a02536b01940407c74b757c6
Edgartheprogrammer <eo.gh@hotmail.com>
2025-03-13 15:17:42 +0100

    in construction page added to jsx and css folders

--- src/components/CatsSlider.jsx
+++ src/components/CatsSlider.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/CatsSlider.module.css'
 
 const CatsSlider = () => {
   return (
---
+++ src/pages/InConstruction.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/InConstruction.module.css'
+
+const InConstruction = () => {
+  return (
+    <div>InConstruction</div>
+  )
+}
+
+export default InConstruction
