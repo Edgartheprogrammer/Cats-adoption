30a537db0abf3a65a00185d020b60cb5e0d722c4
hema <hemapriyaweb@gmail.com>
2025-03-28 21:35:09 +0100

    Added breadcrumbs

--- src/pages/AdoptPage/AdoptPage.jsx
+++ src/pages/AdoptPage/AdoptPage.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import ContactForm from '../../components/ContactForm/ContactForm.jsx';
 import styles from './AdoptPage.module.css';
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs.jsx';
 
 const AdoptPage = () => {
   return (
     <main className={styles['adopt-page-container']}>
       <h1>Contact Us</h1>
+      <Breadcrumbs />
       <ContactForm />
     </main>
   );
